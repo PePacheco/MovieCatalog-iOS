88a5973ac806a31699438ca6bfb9807871a5c353
PePacheco <pedro_pacheco98@outlook.com>
2021-03-25 14:29:51 -0300

    Initial Commit

---
+++ SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Pedro Gomes Rubbo Pacheco on 25/03/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
